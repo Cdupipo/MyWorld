f07d3465e6570c2a84322bb57cee9069c629adf1
Cdupipo <beautiful.sunrise02@gmail.com>
2024-09-02 11:45:15 +0200

    revert

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
-
 let toggle = document.querySelector('.toggle');
 let menu = document.querySelector('.menu');
 toggle.onclick = function(){
