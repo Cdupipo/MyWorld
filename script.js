61dc202b4b35f7b56553f8eb1235c384562f4cbe
Cdupipo <beautiful.sunrise02@gmail.com>
2024-09-02 11:32:13 +0200

    neon

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ neonCursor({
     sleepTimeCoefY: 0.0025
 });
 
+/*
 let toggle = document.querySelector('.toggle');
 let menu = document.querySelector('.menu');
 toggle.onclick = function(){
     menu.classList.toggle('active');
 }
-
+*/
