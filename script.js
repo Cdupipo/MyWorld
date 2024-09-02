8452e78584691b5528b6017dd38b31dd960f841f
Cdupipo <beautiful.sunrise02@gmail.com>
2024-09-02 11:43:26 +0200

    revert

--- script.js
+++ script.js
@@ -5,17 +5,3 @@ let menu = document.querySelector('.menu');
 toggle.onclick = function(){
     menu.classList.toggle('active');
 }
-
-neonCursor({
-    el: document.getElementById('app'),
-    shaderPoints: 16,
-    curvePoints: 80,
-    curveLerp: 0.5,
-    radius1: 5,
-    radius2: 30,
-    velocityTreshold: 10,
-    sleepRadiusX: 100,
-    sleepRadiusY: 100,
-    sleepTimeCoefX: 0.0025,
-    sleepTimeCoefY: 0.0025
-});
