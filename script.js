3457918abfaff53b4ac3ce3d34792040dbfafe72
Cdupipo <beautiful.sunrise02@gmail.com>
2024-09-02 11:40:58 +0200

    Neon

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
 
+let toggle = document.querySelector('.toggle');
+let menu = document.querySelector('.menu');
+toggle.onclick = function(){
+    menu.classList.toggle('active');
+}
+
 neonCursor({
     el: document.getElementById('app'),
     shaderPoints: 16,
@@ -13,11 +19,3 @@ neonCursor({
     sleepTimeCoefX: 0.0025,
     sleepTimeCoefY: 0.0025
 });
-
-/*
-let toggle = document.querySelector('.toggle');
-let menu = document.querySelector('.menu');
-toggle.onclick = function(){
-    menu.classList.toggle('active');
-}
-*/
